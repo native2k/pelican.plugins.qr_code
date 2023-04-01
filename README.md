6537bbf3254538bf5ed2cfc5fad11d0cf54f2aa3
Sven Richter <mail@svenrichter.de>
2023-04-01 21:05:33 +0200

    feat: add settings

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 
 Pelican Plugin to the url of the page as qr code.
 
+## Parameters
 
-## usage
+`QR_IMAGE_FORMAT` which image format should be used (defaults to `png`)
 
-The plugins generates a qr code and adds its to `page.qrcode`
+`QR_IMAGE_URL_DIR` use directory not file as data for code (defaults to `True`)
+
+`QR_IMAGE_SIZE` size of the generated image (defaults to `10`)
+
+`QR_IMAGE_BORDER` size of the image border (defaults to `4`)
+
+`QR_IMAGE_FILL_COLOR` fill color of the image (defaults to `black`)
+
+`QR_IMAGE_BACK_COLOR` background color of the image (defaults to `white`)
+## Usage
+
+The plugins generate a qr code and adds it to `page.qrcode` which can be used in the template.
 
 ```
 <img src="{{ page.qrcode }}" alt="QRCode with the decoded url of the site"/>
--- qr_code.py
+++ qr_code.py
@@ -5,39 +5,53 @@ from io import BytesIO
 import qrcode
 from pelican import signals
 
-
 logger = logging.getLogger(__name__)
 
-QR_IMAGE_FORMAT = "png"
-QR_IMAGE_SIZE = 10
-QR_IMAGE_BORDER = 4
+
+def default_settings():
+    return {
+        "QR_IMAGE_FORMAT": "png",
+        "QR_IMAGE_URL_DIR": True,
+        "QR_IMAGE_SIZE": 10,
+        "QR_IMAGE_BORDER": 4,
+        "QR_IMAGE_FILL_COLOR": "black",
+        "QR_IMAGE_BACK_COLOR": "white",
+    }
 
 
 def addQRCodePage(page_generator, content):
+    logger.debug(
+        f"addQRCodePage called with page_generator: {page_generator} content: {content}"
+    )
 
-    for page in page_generator.pages:
+    settings = default_settings()
+    settings.update(page_generator.settings)
 
-        # build url of the page
-        url = f"{page.get_siteurl()}/{page.save_as}"
+    # build url of the page
+    url = f"{content.get_siteurl()}/{content.save_as}"
+    if settings["QR_IMAGE_URL_DIR"]:
+        url = "/".join(url.split("/")[:-1])
 
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=QR_IMAGE_SIZE,
-            border=QR_IMAGE_BORDER,
-        )
-        qr.add_data(url)
-        qr.make(fit=True)
-        img = qr.make_image(
-            fill_color="black", back_color="white"
-        )
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=settings["QR_IMAGE_SIZE"],
+        border=settings["QR_IMAGE_BORDER"],
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(
+        fill_color=settings["QR_IMAGE_FILL_COLOR"],
+        back_color=settings["QR_IMAGE_BACK_COLOR"],
+    )
 
-        buffer = BytesIO()
-        img.save(buffer, format=QR_IMAGE_FORMAT)
-        page.qrcode = "data:image/{};base64,{}".format(
-            QR_IMAGE_FORMAT.lower(), base64.b64encode(buffer.getvalue()).decode("utf-8")
-        )
-        logger.info(f"Added QRCode ({url}) to {page}")
+    buffer = BytesIO()
+    img.save(buffer, format=settings["QR_IMAGE_FORMAT"])
+    content.qrcode = "data:image/{};base64,{}".format(
+        settings["QR_IMAGE_FORMAT"].lower(),
+        base64.b64encode(buffer.getvalue()).decode("utf-8"),
+    )
+    logger.info(f"Added QRCode ({url}) to {content}")
 
 
 def register():
